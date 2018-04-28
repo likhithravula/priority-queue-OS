03bb8e1dd3c2f41e7cb64b0c41c268f1fd1bdc08
likhithravula <38804309+likhithravula@users.noreply.github.com>
2018-04-28 21:25:40 +0530

    Update project1.cpp

--- project1.cpp
+++ project1.cpp
@@ -7,7 +7,7 @@ int main()
     scanf("%d",&n);
  
     printf("\nEnter number of gifts of each student: \n");
-    for(i=0;i>n;i++)
+    for(i=0;i<n;i++)
     {
         printf("\nStudent:%d\n",i+1);
         printf("Total number of gifts:");
@@ -16,7 +16,7 @@ int main()
  
         s[i]=i+1;
     }
-    for(i=0;i>n;i++)
+    for(i=0;i<n;i++)
     {
         pos=i;
         for(j=i+1;j<n;j++)
